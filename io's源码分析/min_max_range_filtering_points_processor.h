0e4c9901aeb8203782f92b0455217749ef551c2e
learnmoreonce <ushendaiyo@163.com>
2017-06-13 21:24:20 +0800

    0.4

--- io's源码分析/min_max_range_filtering_points_processor.h
+++ io's源码分析/min_max_range_filtering_points_processor.h
@@ -14,12 +14,11 @@
  * limitations under the License.
  */
 
-#ifndef CARTOGRAPHER_IO_FIXED_RATIO_SAMPLING_POINTS_PROCESSOR_H_
-#define CARTOGRAPHER_IO_FIXED_RATIO_SAMPLING_POINTS_PROCESSOR_H_
+#ifndef CARTOGRAPHER_IO_MIN_MAX_RANGE_FILTERING_POINTS_PROCESSOR_H_
+#define CARTOGRAPHER_IO_MIN_MAX_RANGE_FILTERING_POINTS_PROCESSOR_H_
 
 #include <memory>
 
-#include "cartographer/common/fixed_ratio_sampler.h"
 #include "cartographer/common/lua_parameter_dictionary.h"
 #include "cartographer/io/points_processor.h"
 
@@ -27,38 +26,37 @@ namespace cartographer {
 namespace io {
 
 /*
-FixedRatioSamplingPointsProcessor是采样滤波器,只让具有固定采样的点保留,其余全部 remove
+距离滤波器,距离太小和太大都过滤掉.
+
 */
-// Only let a fixed 'sampling_ratio' of points through. A 'sampling_ratio' of 1.
-// makes this filter a no-op.
-class FixedRatioSamplingPointsProcessor : public PointsProcessor {
+// Filters all points that are farther away from their 'origin' as 'max_range'
+// or closer than 'min_range'.
+class MinMaxRangeFiteringPointsProcessor : public PointsProcessor {
  public:
   constexpr static const char* kConfigurationFileActionName =
-      "fixed_ratio_sampler";
-
-  FixedRatioSamplingPointsProcessor(double sampling_ratio,
-                                    PointsProcessor* next);
-
-  static std::unique_ptr<FixedRatioSamplingPointsProcessor> FromDictionary(
+      "min_max_range_filter";
+  MinMaxRangeFiteringPointsProcessor(double min_range, double max_range,
+                                     PointsProcessor* next);
+  static std::unique_ptr<MinMaxRangeFiteringPointsProcessor> FromDictionary(
       common::LuaParameterDictionary* dictionary, PointsProcessor* next);
 
-  ~FixedRatioSamplingPointsProcessor() override{};
+  ~MinMaxRangeFiteringPointsProcessor() override {}
 
-  FixedRatioSamplingPointsProcessor(const FixedRatioSamplingPointsProcessor&) =
-      delete;
-  FixedRatioSamplingPointsProcessor& operator=(
-      const FixedRatioSamplingPointsProcessor&) = delete;
+  MinMaxRangeFiteringPointsProcessor(
+      const MinMaxRangeFiteringPointsProcessor&) = delete;
+  MinMaxRangeFiteringPointsProcessor& operator=(
+      const MinMaxRangeFiteringPointsProcessor&) = delete;
 
   void Process(std::unique_ptr<PointsBatch> batch) override;
   FlushResult Flush() override;
 
  private:
-  const double sampling_ratio_;
+  const double min_range_;
+  const double max_range_;
   PointsProcessor* const next_;
-  std::unique_ptr<common::FixedRatioSampler> sampler_;
 };
 
 }  // namespace io
 }  // namespace cartographer
 
-#endif  // CARTOGRAPHER_IO_FIXED_RATIO_SAMPLING_POINTS_PROCESSOR_H_
+#endif  // CARTOGRAPHER_IO_MIN_MAX_RANGE_FILTERING_POINTS_PROCESSOR_H_
